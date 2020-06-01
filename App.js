05ca6356892276505408b7f68d1075b518d5b8d5
Taylor Wilhite <taylorwilhite@gmail.com>
2020-05-31 17:16:22 -0700

    add extra resource

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const initialState = {
-    stone: 0,
-    miners: 0,
-    minerCost: 10
+    wood: 0,
+    lumberjacks: 0,
+    lumberjackCost: 10,
+    drills: 0,
+    drillCost: 100
   }
   const [state, setState] = useState(initialState);
 
   const tick = () => {
-    const newStone = state.miners / 10
-    setState({...state, stone: state.stone + newStone})
+    const newWood = (state.lumberjacks / 10) + state.drills
+    setState({...state, wood: state.wood + newWood})
   }
 
   useEffect(() => {
@@ -21,26 +23,39 @@ const App = () => {
     return () => clearInterval(timer);
   })
   const mine = () => {
-    setState({...state, stone: state.stone + 1})
+    setState({...state, wood: state.wood + 1})
   }
 
-  const addMiner = () => {
-    if (state.minerCost > state.stone) {
+  const addLumberjack = () => {
+    if (state.lumberjackCost > state.wood) {
       return
     }
     setState({
       ...state,
-      stone: state.stone - state.minerCost,
-      miners: state.miners + 1, minerCost: Math.floor(state.minerCost * 1.25)
+      wood: state.wood - state.lumberjackCost,
+      lumberjacks: state.lumberjacks + 1, lumberjackCost: Math.floor(state.lumberjackCost * 1.25)
+    })
+  }
+  const addDrill = () => {
+    if (state.drillCost > state.wood) {
+      return
+    }
+    setState({
+      ...state,
+      wood: state.wood - state.drillCost,
+      drills: state.drills + 1,
+      drillCost: Math.floor(state.drillCost * 1.25)
     })
   }
   return (
     <div id="App">
       <h1 className="text-teal-500 text-2xl">Hello World!</h1>
-      <p className="text-red-500 text-lg">Count is: {Math.floor(state.stone)}</p>
-      <p className="text-red-500 text-lg">miners is: {state.miners}</p>
+      <p className="text-red-500 text-lg">Count is: {Math.floor(state.wood)}</p>
+      <p className="text-red-500 text-lg">lumberjacks: {state.lumberjacks}</p>
+      <p className="text-red-500 text-lg">drills: {state.drills}</p>
       <button className="bg-teal-500 text-white px-2 py-1 rounded-lg mx-2" onClick={() => mine()}>Mine!</button>
-      <button className="bg-teal-500 text-white px-2 py-1 rounded-lg mx-2" onClick={() => addMiner()}>Add Miner {state.minerCost}</button>
+      <button className="bg-teal-500 text-white px-2 py-1 rounded-lg mx-2" onClick={() => addLumberjack()}>Add Lumberjack {state.lumberjackCost}</button>
+      <button className="bg-teal-500 text-white px-2 py-1 rounded-lg mx-2" onClick={() => addDrill()}>Add Drill {state.drillCost}</button>
     </div>
   )
 }
